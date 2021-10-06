d35df1d818f72798f4088898f6fd0af4a0f0d771
Roman <ostapenkoua@gmail.com>
2021-10-06 16:20:43 +0300

    закончен урок

---
+++ script.js
@@ -0,0 +1,86 @@
+    "use strict";
+    // console.log('arr' + ' - object');
+
+    // console.log(4 + +'5'); //с
+
+    // let incr = 10,
+    //     decr = 10;
+
+    // // ++incr; //оператор инкремента
+    // // --decr; // оператор дикремента
+
+    // console.log(++incr); //постфик префиксный и постфиксный
+    // console.log(decr--); // постфиксный оператор
+    // console.log(decr);
+
+    console.log(78 % 33); //возращает остаток от деление
+
+    // console.log(2 + 2 * 2 != 6);
+
+    // && // все правда
+    // || // один правда
+
+    let isChecked, isClosed;
+
+    isChecked = false;
+    isClosed = false;
+
+    console.log(isChecked || !isClosed);
+
+
+
+
+
+
+
+
+
+    // alert('Hello');
+    // const result = confirm('Are you here?');
+    // console.log(result);
+
+    // const answer = +prompt('Вам есть 18?', '18');
+    // console.log(answer + 5); //вся информация будет как строка
+
+    // const answer = [];
+
+    // answer[0] = prompt('Как ваше имя?', '');
+    // answer[1] = prompt('Как ваше фамилия', '');
+    // answer[2] = prompt('Сколько вам лет?', '');
+
+    // console.log(typeof(answer));
+
+    // console.log(typeof(null));
+
+
+
+
+
+    // let number = 4.6; // изменяемая переменная
+
+    // /*    console.log(-4 / 0);
+    //    console.log('strong' * 9);
+
+    //    const persone = 'Alex name';
+    //    console.log(persone);
+
+    //    const bool = true;
+    //    const bool1 = false;
+
+    //    console.log(someting); */
+
+    // /*  let und;
+
+    //  console.log(und); */
+
+    // const obj = {
+    //     name: "John",
+    //     age: 25,
+    //     isMarried: false
+    // };
+
+    // // console.log(obj.name)
+    // console.log(obj['age']);
+
+    // let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}];
+    // console.log(arr[1]);
